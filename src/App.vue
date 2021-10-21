aaa2119eb2bd3fbbb705f7a2293851ad306699d0
Parker Byers <parker.n.byers@gmail.com>
2021-10-21 15:53:41 -0500

    added smaller navbar functionality, also changed styling a little bit

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,20 @@
           <strong>TOA</strong>
         </router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-end">
 
           <router-link to="/createevent" class="navbar-item">Create Event</router-link>
           <router-link to="/exploreevents" class="navbar-item">Explore</router-link>
-          
+          <router-link to="/log-in" class="navbar-item">Log in</router-link>
 
-          <div class="navbar-item">
-            <div class="buttons">
-              <router-link to="/log-in" class="button is-light">Log in</router-link>
-            </div>
-          </div>
         </div>
       </div>
     </nav>
@@ -34,10 +29,20 @@
     </section>
 
     <footer class="footer">
-      <p class="has-text-centered">insert some links here</p>
+      <p class="has-text-centered">insert some links here, in the footer</p>
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
