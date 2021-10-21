77ba55262085bbeed34c7cc387acf1610469b41f
Parker Byers <parker.n.byers@gmail.com>
2021-10-21 15:35:55 -0500

    made navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <nav class="navbar is-warning">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>TOA</strong>
+        </router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+
+          <router-link to="/createevent" class="navbar-item">Create Event</router-link>
+          <router-link to="/exploreevents" class="navbar-item">Explore</router-link>
+          
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">insert some links here</p>
+    </footer>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-@import '../node_modules/bulma'
+@import '../node_modules/bulma';
 </style>
