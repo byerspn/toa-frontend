7ce0d7e74c258b3e4d3aced77c7cf5e3b01b42fc
Parker Byers <parker.n.byers@gmail.com>
2021-10-23 18:51:26 -0500

    messed up from reinstalling npm and node, this is the restart

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,30 @@
 <template>
-  <div id="wrapper">
-    <nav class="navbar is-warning">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">
-          <strong>TOA</strong>
-        </router-link>
-
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
-        <div class="navbar-end">
-
-          <router-link to="/createevent" class="navbar-item">Create Event</router-link>
-          <router-link to="/exploreevents" class="navbar-item">Explore</router-link>
-          <router-link to="/log-in" class="navbar-item">Log in</router-link>
-
-        </div>
-      </div>
-    </nav>
-
-    <section class="section">
-      <router-view/>
-    </section>
-
-    <footer class="footer">
-      <p class="has-text-centered">insert some links here, in the footer</p>
-    </footer>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-export default {
-  data() {
-    return{
-      showMobileMenu: false,
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
-</script>
-
-<style lang="scss">
-@import '../node_modules/bulma';
 </style>
