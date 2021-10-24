8f959e7330fbdcceef349c41828797d165653d73
Parker Byers <parker.n.byers@gmail.com>
2021-10-23 20:19:47 -0500

    navbar dropdown mobile menu works i think

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@
         <router-link to="/" class="navbar-item">
           <strong>TOA</strong>
         </router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
-        <div class="navbar-start">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-end">
           <router-link to="/events" class="navbar-item">Browse Events</router-link>
           <router-link to="/create-event" class="navbar-item">Create Event</router-link>
-          <router-link to="/log-in" class="navbar-item is-primary">Log in</router-link>
-          <router-link to="/log-in" class="navbar-item is-primary">Log in</router-link>
+          <router-link to="/log-in" class="navbar-item">Log in</router-link>
+          <router-link to="/log-in" class="navbar-item">Log out</router-link>
         </div>
       </div>
     </nav>
@@ -33,6 +33,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showMobileMenu: false,
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
