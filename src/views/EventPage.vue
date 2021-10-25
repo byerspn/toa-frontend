15fd16efc41bb7658c0c067586b2d4c8ba0bbeac
Parker Byers <parker.n.byers@gmail.com>
2021-10-25 04:30:22 -0500

    registration working again. right now.

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="event-page">
-        <h1>This is the single event page</h1>
-        <h2>This is the id url param: {{ $route.params.id }}</h2>
+        <!-- <h1>This is the single event page</h1> -->
+        <!-- <h2>This is the id url param: {{ $route.params.id }}</h2> -->
 
         <div class="columns is-multiline">
             <!-- columns areas are divided into 12. i dont want text to touch edges of screen so i add 1/12 spacers and give the actual content 10/12 space. -->
@@ -20,7 +20,7 @@
             <div class="column is-1"></div>
             <div class="column is-10 has-text-centered">
                 <button @click="register()" class="is-size-4" v-if="$store.state.isAuthenticated">Register for Event</button>
-                <router-link to="/signin"><button @click="register()" class="is-size-4" v-if="!$store.state.isAuthenticated">Sign in to register for event</button></router-link>
+                <router-link to="/signin"><button class="is-size-4" v-if="!$store.state.isAuthenticated">Sign in to register for event</button></router-link>
                 <p class="is-size-4">Entrants:</p>
                 <div 
                     v-for="entrant in singleEvent.entrants" 
@@ -30,6 +30,10 @@
                 </div>
             </div>
             <div class="column is-1"></div>
+            <div class="column is-1"></div>
+            <div class="column is-10">
+                <a v-bind:href="bracketlink" target="_blank">bracket style</a>
+            </div>
         </div>
     </div>
 </template>
@@ -42,7 +46,9 @@
         data() {
             return {
                 singleEvent: {},
-                success: false
+                success: false,
+                entrants: 0,
+                bracketlink: ''
             }
         },
         components: {
@@ -57,14 +63,22 @@
                     .get(`/events/${ this.$route.params.id }`)
                     .then(res => {
                         this.singleEvent = res.data
+                        const tempEntrantVar = this.singleEvent.entrants
+                        this.entrants = tempEntrantVar.length
+                        this.bracketlink = `https://www.printyourbrackets.com/fillable-brackets/${this.entrants}-team-single-fillable.pdf`
                     })
                     .catch(error => {
                         console.log(error)
                     })
             },
             register() {
-                const updatedEntrants =  this.singleEvent.entrants
+                const originalEntrants = this.singleEvent.entrants
+                const updatedEntrants = originalEntrants.map(originalEntrant => {
+                    return originalEntrant._id
+                })
                 updatedEntrants.push(this.$store.state.userId)
+                console.log('userid pushed to updated entrants')
+                console.log(updatedEntrants)
 
                 const eventRegistered = {
                     name: this.singleEvent.name,
@@ -79,6 +93,7 @@
                     .then(res => {
                         this.success = true
                         this.getSingleEvent()
+                        console.log('updated with reg')
                     })
                     .catch(error => {
                         // console.log('catch of eventRegistered()')
